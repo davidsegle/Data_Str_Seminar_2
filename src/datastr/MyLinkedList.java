e7efc94705393c54783ac4b1e325f9df22a4caa6
Dāvids Egle <s24egledavi@vea.lv>
2026-03-31 11:38:42 +0300

    Add: MyLinkedList class with variables, isEmpty, isFull

---
+++ src/datastr/MyLinkedList.java
@@ -0,0 +1,53 @@
+package datastr;
+
+public class MyLinkedList<Ttype> {
+	private MyNode<Ttype> firstNode = null;
+	private MyNode<Ttype> lastNode = null;
+	private int howManyElements = 0;
+	
+	public int getHowManyElements() {
+		return howManyElements;
+	}
+	
+	public boolean isEmpty() {
+		return (howManyElements == 0);
+	}
+	
+	public boolean isFull() {
+		try
+		{
+			new MyNode<Character>('A');
+			return false;
+		}
+		catch (OutOfMemoryError e) {
+			return true;
+		}
+	}
+	
+	public void add(Ttype element) throws Exception{
+		if(isFull()) {
+			throw new Exception("Saraksts ir pilns un nav iespejams pievienot elementu");
+		}
+		if(element == null) {
+			throw new Exception("Padotais elements nav noradits");
+		}
+		if(isEmpty()) {
+			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+			lastNode = newNode;
+			firstNode = newNode;
+			howManyElements++;
+		}
+		else 
+		{
+			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+			lastNode.setNextNode(newNode);
+			newNode.setPreviousNode(lastNode);
+			
+			lastNode = newNode;
+			howManyElements++;
+		}
+		
+		
+	}
+	
+}
