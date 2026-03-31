3fc96d159ad9afd72a9c9eb182acdde12967edeb
Dāvids Egle <s24egledavi@vea.lv>
2026-03-31 11:04:31 +0300

    Add: added MyNode class with variables, getters, setters, construcotr and toString

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,49 @@
 package datastr;
 
-public class MyNode {
-
+public class MyNode<Ttype> {
+	private Ttype element;
+	private MyNode<Ttype> nextNode = null;
+	private MyNode<Ttype> previousNode = null;
+	
+	public Ttype getElement() {
+		return element;
+	}
+	
+	public MyNode<Ttype> getNextNode() {
+		return nextNode;
+	}
+	
+	public MyNode<Ttype> getPreviousNode() {
+		return previousNode;
+	}
+	
+	public void setElement(Ttype inputElement) {
+		if(inputElement != null) {
+			element = inputElement;
+		}
+		else {
+			element = (Ttype)new Object();
+		}
+	}
+	
+	public void setNextNode(MyNode<Ttype> nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	public void setPreviousNode(MyNode<Ttype> previousNode) {
+		this.previousNode = previousNode;
+	}
+	
+	public MyNode(Ttype inputElement) {
+		setElement(inputElement);
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
+	
+	
+	
+	
+	
 }
